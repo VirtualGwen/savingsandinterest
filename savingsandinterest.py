67b8b1b3d896dd21668b9634a8c7725ab9fca9f3
VirtualGwen <gvirtue@me.com>
2021-05-17 09:33:51 -0400

    Create savingsandinterest.py

---
+++ savingsandinterest.py
@@ -0,0 +1,14 @@
+'''Program that calculates savings and interest'''
+
+initial_savings = 10000
+interest_rate = 0.05
+
+years = int(input('Enter years: '))
+print()
+
+savings = initial_savings
+for i in range(years):
+    print(' Savings in year {}: ${:.2f}'.format(i, savings))
+    savings = savings + (savings*interest_rate)
+
+print('\n')
